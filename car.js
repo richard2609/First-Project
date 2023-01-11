15c915acadccb28ed8896e11a387a9a181306afe
richy roby <richardsojan2609@gmail.com>
2023-01-11 10:55:46 -0600

    adding new files

---
+++ car.js
@@ -0,0 +1,10 @@
+var age;
+var age=prompt("enter your age");
+if(Number(age)>=18){
+    alert("you can drive! enjoy!");
+}
+else(Number(age)<18);{
+    alert("you cannot drive! get off you bitch!");
+}
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+alert(array2[1][1][0]);
